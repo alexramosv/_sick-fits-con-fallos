03d5dfcc3e706884a7b216205d30158296c819af
Alex Ramos <alex@example.com>
2019-01-07 14:39:20 +0100

    Añadido index.js y Sell.js

---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "next/link";
+const Home = props => (
+  <div>
+    <p>Home!!</p>
+    <Link href="/Sell">Sell !!</Link>
+  </div>
+);
+
+export default Home;
