cbcaf555f18341ff71f8459442420c7fec87b412
Alex Ramos <alex@example.com>
2019-01-08 14:42:40 +0100

    WIP: Aplicando estilos

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,17 +1,45 @@
 import React, { Component } from "react";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
 
 import Header from "./Header";
 import Nav from "./Nav";
 import Meta from "./Meta";
 
+const theme = {
+  red: "#FF0000",
+  black: "#393939",
+  grey: "#3A3A3A",
+  lightgrey: "#E1E1E1",
+  offWhite: "#EDEDED",
+  maxWidth: "1000px",
+  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+const MyList = styled.button`
+  background-color: ${props => (props.red ? `red` : `greenyellow`)};
+  font-size: 100px;
+`;
+
 class Page extends Component {
   render() {
     return (
-      <div>
+      <ThemeProvider theme={theme}>
+        <StyledPage />
         <Meta />
         <Header />
-        <Nav />
-      </div>
+        <Inner>{this.props.children}</Inner>
+      </ThemeProvider>
     );
   }
 }
