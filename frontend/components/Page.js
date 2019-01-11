aced62be34485457403aaec38447415c8609b753
Alex Ramos <alex@example.com>
2019-01-11 14:25:41 +0100

    WIP: Commit inicial antes de empezar el GraphQL

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import styled, { ThemeProvider, injectGlobal } from "styled-components";
 
 import Header from "./Header";
-import Nav from "./Nav";
 import Meta from "./Meta";
 
 const theme = {
@@ -26,6 +25,33 @@ const Inner = styled.div`
   padding: 2rem;
 `;
 
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
+
 const MyList = styled.button`
   background-color: ${props => (props.red ? `red` : `greenyellow`)};
   font-size: 100px;
@@ -35,13 +61,85 @@ class Page extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <StyledPage />
-        <Meta />
-        <Header />
-        <Inner>{this.props.children}</Inner>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
       </ThemeProvider>
     );
   }
 }
 
 export default Page;
+/*
+
+import React, { Component } from "react";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
+import Header from "./Header";
+import Meta from "./Meta";
+
+const theme = {
+  red: "#FF0000",
+  black: "#393939",
+  grey: "#3A3A3A",
+  lightgrey: "#E1E1E1",
+  offWhite: "#EDEDED",
+  maxWidth: "1000px",
+  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
+
+class Page extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
+    );
+  }
+}
+
+export default Page;
+*/
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Home = props => (
   <div>
-    <h1>Home!!</h1>
+    <h2>Home!!</h2>
   </div>
 );
 
