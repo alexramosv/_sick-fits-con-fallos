d42b4b086646ecbdb761d339ca218bfd985674f5
Alex Ramos <alex@example.com>
2019-01-17 14:31:50 +0100

    Añadido `createServer.js`, `db.js` y `schema.graphql`

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
 
-module.exports = mutations;
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    return newDog;
+  }
+};
+
+module.exports = Mutations;
