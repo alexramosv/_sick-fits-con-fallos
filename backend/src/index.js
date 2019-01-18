00254ff53a97407feeec6b19fe6b5fd258cdee46
Alex Ramos <alex@example.com>
2019-01-18 14:37:02 +0100

    Finalizada la parte del servidor GraphQl y empezando con el cliente

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
-// let's go!
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
 const db = require("./db");
 
 const server = createServer();
+
+// TODO Use express middlware to handle cookies (JWT)
+// TODO Use express middlware to populate current user
 
 server.start(
   {
@@ -13,6 +15,6 @@ server.start(
     }
   },
   deets => {
-    console.log(`Server is now running on port http:/localhost:${deets.port}`);
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
 );
